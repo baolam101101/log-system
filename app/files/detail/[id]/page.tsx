094bbcd27a390e929d16d0a9957b22df9893b05d
baolam101101 <baolam101101@gmail.com>
2024-04-23 15:07:34 +0700

    done params to id

--- app/files/detail/[id]/page.tsx
+++ app/files/detail/[id]/page.tsx
@@ -1,118 +1,15 @@
-/* eslint-disable @next/next/no-async-client-component */
-// "use client";
-
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-// import Link from "next/link";
-// import { useRouter } from "next/router";
-
-// const FileDetail = () => {
-//   const router = useRouter();
-//   const { fileId } = router.query;
-
-//   const [fileData, setFileData] = useState(null);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       setIsLoading(true);
-//       setError(null);
-
-//       try {
-//         const response = await axios.get(`/api/files/${fileId}`);
-//         setFileData(response.data);
-//       } catch (err) {
-//         console.log(err);
-//         setError(err.message || "An error occurred!");
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     } ;
-
-//     fetchData();
-// }, [fileId]);
-
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div className="error">Error: {error}</div>
-//   }
-
-//   if (!fileData) {
-//     return <p>No file found with ID: {fileId}</p>;
-//   }
-
-//   return (
-//     <div className="container mx-auto">
-//       <h1 className="text-2xl font-bold mb-4 text-center">File Details</h1>
-//       <div className="flex flex-col mb-4">
-//         <p className="font-bold">Name:</p>
-//         <p>{fileData.name}</p>
-//       </div>
-//       <div className="flex flex-col mb-4">
-//         <p className="font-bold">Type:</p>
-//         <p>{fileData.type}</p>
-//       </div>
-//       <div className="flex flex-col mb-4">
-//         <p className="font-bold">Date Time:</p>
-//         <p>{fileData.datetime}</p>
-//       </div>
-//       <div className="flex flex-col mb-4">
-//         <p className="font-bold">Storage Size:</p>
-//         <p>{fileData.storageSize} bytes</p>
-//       </div>
-//       <div className="flex flex-col mb-4">
-//         <p className="font-bold">Created By:</p>
-//         <p>{fileData.createdBy}</p>
-//       </div>
-//       <Link href="/files">
-//         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//           Back to List
-//         </button>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default FileDetail;
-
-// 'use client';
-
-// async function getData() {
-//   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data')
-//   }
-
-//   return res.json()
-// }
-
-// export default async function FileDetails() {
-//   const data = await getData()
-//   console.log("Hello", data)
-//   return (
-//     <div>
-
-//     </div>
-//   )
-// }
-
 "use client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
-function FileDetails( {id} ) {
+function FileDetails( {params}: any ) {
   const [fileDetails, setFileDetails] = useState(null);
   useEffect(() => {
     const fetchFileDetails = async () => {
       try {
         const {data} = await axios.get(
-          `https://jsonplaceholder.typicode.com/posts/${id}`
+          `https://jsonplaceholder.typicode.com/posts/${params.id}`
         );
         // const res = await axios.get(`/api/files/${id}`);
         setFileDetails(data);
@@ -120,11 +17,11 @@ function FileDetails( {id} ) {
         console.error(error);
       }
     };
-    if (id) {
+    if (params.id) {
       fetchFileDetails();
     }
     
-  }, [id]);
+  }, [params.id]);
 
   return (
     <div>
